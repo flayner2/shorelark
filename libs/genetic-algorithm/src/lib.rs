e90e87336fb6b23b275a212f86ba0236b67339d7
flayner2 <flayner5@gmail.com>
2021-04-09 17:25:16 -0300

    Started working on the genetic algorithm

--- libs/genetic-algorithm/src/lib.rs
+++ libs/genetic-algorithm/src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use rand::RngCore;
+
+pub struct GeneticAlgorithm;
+
+impl GeneticAlgorithm {
+    pub fn new() -> Self {
+        Self
     }
+
+    pub fn evolve<I>(&self, population: &[I]) -> Vec<I> {
+        assert!(!population.is_empty());
+
+        (0..population.len())
+            .map(|_| {
+                // crossover
+                // selection
+                // mutation
+                todo!()
+            })
+            .collect()
+    }
+}
+
+pub trait SelectionMethod {
+    fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
+    where
+        I: Individual;
 }
